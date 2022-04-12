3bcf0b19eeecb8341f9563fd565c992c22e68324
Victor Colombet <victorcolombet@gmail.com>
2022-04-12 17:16:02 +0200

    Création des modèles

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class disks_artist(models.Model):
+    id = models.IntegerField()
+    name = models.CharField()
+
+class disks_album(models.Model):
+    id = models.IntegerField()
+    title = models.CharField()
+    artist_id = models.IntegerField()
+    
+class disks_track(models.Model):
+    id = models.IntegerField()
+    name = models.CharField()
+    miliseconds = models.IntegerField()
+    bytes = models.IntegerField()
+    unitPrice = models.IntegerField()
+    album_id = models.IntegerField()
+    composer = models.CharField()
+    
