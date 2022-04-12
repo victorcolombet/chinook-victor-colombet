3557df0ef9f93a07e523569c429a019cc42e1c92
Victor Colombet <victorcolombet@gmail.com>
2022-04-12 22:25:04 +0200

    Correction bug models

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
 class disks_artist(models.Model):
-    id = models.IntegerField()
-    name = models.CharField()
+    id = models.IntegerField(default = 1, primary_key=True)
+    name = models.CharField(max_length=200)
 
 class disks_album(models.Model):
-    id = models.IntegerField()
-    title = models.CharField()
-    artist_id = models.IntegerField()
+    id = models.IntegerField(default = 1, primary_key=True)
+    title = models.CharField(max_length=200)
+    artist_id = models.IntegerField(default = 1)
     
 class disks_track(models.Model):
-    id = models.IntegerField()
-    name = models.CharField()
-    miliseconds = models.IntegerField()
-    bytes = models.IntegerField()
-    unitPrice = models.IntegerField()
-    album_id = models.IntegerField()
-    composer = models.CharField()
+    id = models.IntegerField(default = 1, primary_key=True)
+    name = models.CharField(max_length=200)
+    miliseconds = models.IntegerField(default = 0)
+    bytes = models.IntegerField(default = 0)
+    unitPrice = models.IntegerField(default = 0)
+    album_id = models.IntegerField(default = 0)
+    composer = models.CharField(max_length=200)
     
